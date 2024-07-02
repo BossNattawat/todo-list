fc787dce5f44c245bada292c1be74b8664303a62
Nattawat <75540581+BossNattawat@users.noreply.github.com>
2024-07-02 20:45:46 +0700

    Update Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
     return (
       <div className="Todo">
           <p className={`${task.completed ? "completed" : "incompleted"}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
-          <div>
-          <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} onClick={() => editTodo(task.id)} />
-          <FontAwesomeIcon className="delete-icon" icon={faTrash} onClick={() => deleteTodo(task.id)} />
+          <div className='edit-bin'>
+            <img src="./edit.png" alt='edit' className="edit-icon" onClick={() => editTodo(task.id)}/>
+            <img src="./bin.png" alt='bin' className="delete-icon" onClick={() => deleteTodo(task.id)}/>
           </div>
       </div>
     )
